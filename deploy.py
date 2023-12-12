958859ef7dc2a2ec298fddbd56b5485e4e054ccd
ShivaniBandapalli <shivanigoud@gmail.com>
2023-12-12 20:39:44 +0530

    first commit

--- deploy.py
+++ deploy.py
@@ -1,22 +1,38 @@
-from flask import Flask ,render_template,request
-import pickle
+from flask import Flask, request, render_template
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+import joblib as joblib
+import os
 
-app=Flask(__name__)
+model = joblib.load('saved_model1.pkl')
+scaler = joblib.load('scaler.save')
 
-model=pickle.load(open('saved_model.sav','rb'))
+app = Flask(__name__)
 
-@app.route('/submit')
+IMG_FOLDER = os.path.join('static', 'IMG')
+app.config['UPLOAD_FOLDER'] = IMG_FOLDER
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    result=''
-    return render_template('index.html',**locals())
+    if request.method == 'POST':
+        sl = request.form['SepalLength']
+        sw = request.form['SepalWidth']
+        pl = request.form['PetalLength']
+        pw = request.form['PetalWidth']
+        data = np.array([[sl, sw, pl, pw]], dtype=float)
+        x = scaler.transform(data)
+        prediction = model.predict(x)
+        image = prediction[0] + '.png'
+        image = os.path.join(app.config['UPLOAD_FOLDER'], image)
+        return render_template('index.html', prediction=prediction[0], image=image)
+    return render_template('index.html')
 
-@app.route('/predict',methods=['POST','GET'])
-def predict():
-    sepal_length=float(request.form['sepal_length'])
-    sepal_width=float(request.form['sepal_width'])
-    petal_length =float(request.form['petal_width'])
-    petal_width=float(request.form['petal_width'])
-    result=model.predict([[sepal_length,sepal_width,petal_length,petal_width]])[0]
-    return render_template('index.html',**locals())
-if __name__=='__main__':
-    app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
